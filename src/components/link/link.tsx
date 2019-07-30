e5ac22d0e003a11243418a397a9adc3714ed6113
Owen Riley <oriley@lucidchart.com>
2019-07-30 13:38:22 -0600

    Link defines a color and hover color

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,20 +1,39 @@
+/** @jsx jsx */
 import React from 'react';
+import { jsx } from '@emotion/core';
+import { withTheme } from 'emotion-theming';
 
 interface Props {
 	primary?: boolean,
 	secondary?: boolean,
 	inverse?: boolean,
-	asButton?: boolean
+	asButton?: boolean,
+	css?: any,
+	theme: any
 }
 
 type Link = Props & React.HTMLProps<HTMLAnchorElement> & React.HTMLAttributes<HTMLAnchorElement>;
 
-const Link = ({ children, primary, secondary, inverse, asButton, ...rest }: Link) => {
-    return (
-		<a {...rest}>
+const Link = withTheme(({ children, primary, secondary, inverse, asButton, theme, ...rest }: Link) => {
+	const linkColor = theme.colors.primary;
+	const linkHoverColor = theme.colors.secondary;
+
+	return (
+		<a
+			css={{
+				color: linkColor,
+				':visited': {
+					color: linkColor
+				},
+				':hover,:focus': {
+					color: linkHoverColor
+				}
+			}}
+			{...rest}
+		>
 			{children}
 		</a>
 	);
-};
+});
 
 export default Link;
