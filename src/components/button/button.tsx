bd1f7864083eef8450f3f81e3a90b120720060bd
Emilie Martinez <emilieknecht@lucidchart.com>
2019-07-31 09:19:40 -0600

    buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,15 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import React, { ReactNode } from "react";
 
 import { withTheme } from 'emotion-theming';
+import { ThemeInterface } from "../../theme/theme";
+
+interface Styles {
+  color?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+}
 
 interface Props {
   className?: string;
@@ -20,6 +29,9 @@ interface Props {
 
   type?: 'button' | 'submit' | 'reset';
 
+  css?: any,
+  theme: ThemeInterface
+
 
   onClick?: () => void;
   onHover?: () => void;
@@ -28,7 +40,7 @@ interface Props {
 
 type Button = Props & React.HTMLProps<HTMLButtonElement> & React.HTMLAttributes<HTMLButtonElement>;
 
-const Button = withTheme(({
+const Button = ({
   className="",
   id="",
   children,
@@ -47,33 +59,77 @@ const Button = withTheme(({
   hover,
   active,
 
+  theme,
+
   type = "button",
   ...rest
 }: Button) => {
-  const getClasses = () => {
-    let emphasis = "default";
-    if (primary) {
-      emphasis = "is-primary";
-    } else if (secondary) {
-      emphasis = "is-secondary";
-    }
+  let color= theme.colors.black;
+  let backgroundColor= theme.colors.white;
+  let border= theme.buttons.border;
+  let boxshadow= theme.buttons.boxShadow;
+  let hoverColor= theme.colors.black;
+  let hoverBackgroundColor= theme.colors.gray;
+  let hoverBorder= theme.colors.black;
+  let hoverBoxShadow = theme.buttons.hoverBoxShadow;
+  let transition = theme.buttons.transition;
 
-    return `${className} ${emphasis}${inverse ? '-inverse' : ''}  ${disabled ? 'is-disabled' : ''}  ${hover ? 'is-hover' : ''} ${active ? 'is-active' : ''} ${asLink ? 'is-link' : ''} ${block ? 'block' : ''}`;
+  if (primary){
+    color = theme.colors.white;
+    backgroundColor = theme.buttons.primary.backgroundColor;
+    border = theme.buttons.primary.border;
+
+    hoverColor= theme.buttons.primary.hoverColor;
+    hoverBackgroundColor= theme.buttons.primary.hoverBackgroundColor;
+    hoverBorder= theme.buttons.primary.hoverBorder;
+  } else if (secondary) {
+    color = theme.buttons.secondary.color;
+    backgroundColor = theme.buttons.secondary.backgroundColor;
+    border = theme.buttons.secondary.border;
+
+    hoverColor = theme.buttons.primary.hoverColor;
+    hoverBackgroundColor= theme.buttons.primary.hoverBackgroundColor;
+    hoverBorder = theme.buttons.primary.hoverBorder;
+  }
+
+  const css = {
+    padding:  `${theme.space[2]}px ${theme.space[4]}px`,
+    fontSize: `${theme.fontSizes[0]}px`,
+    fontWeight: theme.fontWeights.bolder,
+    borderRadius: `${theme.borderRadius}px`,
+    boxShadow: boxshadow,
+    transition: transition,
+
+    color: color,
+    backgroundColor: backgroundColor,
+    border: border,
+
+    '&:hover': {
+      color: hoverColor,
+      backgroundColor: hoverBackgroundColor,
+      border: hoverBorder,
+      cursor: 'pointer',
+      boxShadow: hoverBoxShadow
+    }
+  }
+
+  const getClasses = () => {
+    return `${className}  ${hover ? 'is-hover' : ''} ${active ? 'is-active' : ''}  ${asLink ? 'is-link' : ''}  ${block ? 'block' : ''}`;
   };
 
   return (
     <button
       {...rest}
       className={getClasses()}
-      disabled={disabled}
       onClick={onClick}
       onMouseOver={onHover}
       type={type}
       id={id}
+      css={css}
     >
       {children}
     </button>
   );
-});
+};
 
-export default Button;
+export default withTheme(Button);
