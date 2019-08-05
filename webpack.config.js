712aabff3ec763b6f23d16aa1378821ded2f0656
Lars Bucio <lbucio@lucidchart.com>
2019-08-04 18:49:13 -0600

    feat: Use modules

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
   },
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    library: "",
+    libraryTarget: "commonjs"
   }
 };
