1bb05086bf0360e2a0258d70d02864b6adbe5080
Lars Bucio <lbucio@lucidchart.com>
2019-08-04 18:41:54 -0600

    feat: Use webpack

--- dist/index.ts
+++
@@ -1,9 +0,0 @@
-import Card from "./components/card/card";
-import Button from "./components/button/button";
-import Link from "./components/link/link";
-
-export {
-   Button,
-   Card,
-   Link 
-};
