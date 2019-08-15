9735330d287fa812cc9214575db1bed9a644f625
Lars Bucio <lbucio@lucidchart.com>
2019-08-15 09:26:50 -0600

    feat: Setup temp icons

--- src/lib/components/icon/icon.tsx
+++ src/lib/components/icon/icon.tsx
@@ -1,27 +1,44 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
 import './icon.scss';
 
 enum IconType {
     ArrowRight,
-    ArrowLeft
+    ArrowLeft,
+    CarotRight,
+    CarotDown,
+    Play
 }
 
 interface Props {
+    className?: string;
     type: IconType;
 }
 
-const Icon = ({ type }: Props) => {
+const Icon = ({ className, type }: Props) => {
+    const iconCss = css({
+        height: '100%',
+        width: '100%'
+    });
+
     let icon;
     switch (type) {
         case IconType.ArrowRight:
             icon = <div>&rightarrow;</div>;
+            break;
+        case IconType.CarotRight:
+            icon = <div>&gt;</div>;
+            break;
+        case IconType.CarotDown:
+            icon = <div style={{transform: 'rotate(90deg)'}}>&gt;</div>;
             break;
         default:
             icon = <svg></svg>;
             break;
     }
 
-    return (<div className="icon">{icon}</div>);
+    return (<div css={iconCss} className={className}>{icon}</div>);
 };
 
 export default Icon;
