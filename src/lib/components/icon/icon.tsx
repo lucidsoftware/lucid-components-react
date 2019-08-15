5cf0c77559420d0fd233b65414baaa16f517e6da
Lars Bucio <lbucio@lucidchart.com>
2019-08-15 10:55:21 -0600

    feat: Update Icon carot right

--- src/lib/components/icon/icon.tsx
+++ src/lib/components/icon/icon.tsx
@@ -34,7 +34,15 @@ const Icon = ({ className, type }: Props) => {
             </svg>;
             break;
         case IconType.CarotRight:
-            icon = <div>&gt;</div>;
+            icon = <svg version="1.1" x="0px" y="0px"
+                viewBox="0 0 24.7 39.2">
+                <polyline style={{
+                    fill: 'none',
+                    stroke: '#CCCCCC',
+                    strokeWidth: 7,
+                    strokeMiterlimit: 10
+                }} points="2.5,2.5 19.7,19.5 2.5,36.7 " />
+            </svg>;
             break;
         case IconType.CarotDown:
             icon = <div style={{transform: 'rotate(90deg)'}}>&gt;</div>;
