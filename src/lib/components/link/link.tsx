806fa559d1b26f2791c6414d4f2b3c51568b1b69
Lars Bucio <lbucio@lucidchart.com>
2019-09-09 15:49:27 -0600

    feat: Lint files

--- src/lib/components/link/link.tsx
+++ src/lib/components/link/link.tsx
@@ -8,104 +8,100 @@ import { getButtonStyles } from '../button/button';
 export type UnderlineType = 'none' | 'hover' | 'always';
 
 interface Props {
-	disabled?: boolean,
-	primary?: boolean,
-	secondary?: boolean,
-	inverse?: boolean,
-	asButton?: boolean,
-	block?: boolean,
-	underline?: UnderlineType,
-	css?: any;
+  disabled?: boolean;
+  primary?: boolean;
+  secondary?: boolean;
+  inverse?: boolean;
+  asButton?: boolean;
+  block?: boolean;
+  underline?: UnderlineType;
+  css?: any;
 }
 
 export enum LinkVariant {
-	Default = 'default',
-	Primary = 'primary',
-	Secondary = 'secondary'
+  Default = 'default',
+  Primary = 'primary',
+  Secondary = 'secondary'
 }
 
-export const getLinkStyles = (
-	{
-		block,
-		inverse = false,
-		theme,
-		underline = 'none',
-		variant = LinkVariant.Default,
-	} : {
-		block: boolean
-		inverse: boolean,
-		theme: ThemeProps["theme"],
-		underline: string,
-		variant: LinkVariant,
+export const getLinkStyles = ({
+  block,
+  inverse = false,
+  theme,
+  underline = 'none',
+  variant = LinkVariant.Default
+}: {
+  block: boolean;
+  inverse: boolean;
+  theme: ThemeProps['theme'];
+  underline: string;
+  variant: LinkVariant;
 }) => {
-	const linkUnderline = underline === 'always' ? 'underline' : 'none';
-	const linkUnderlineHover = underline === 'hover' || underline === 'always' ? 'underline' : 'none';
+  const linkUnderline = underline === 'always' ? 'underline' : 'none';
+  const linkUnderlineHover =
+    underline === 'hover' || underline === 'always' ? 'underline' : 'none';
 
-	const linkType = inverse ? 'inverse' : 'default';
-	let { color, hover, disabled } = theme.links[variant][linkType];
-	const display = block ? 'block' : '';
+  const linkType = inverse ? 'inverse' : 'default';
+  const { color, hover, disabled } = theme.links[variant][linkType];
+  const display = block ? 'block' : '';
 
-	const css = {
-		color,
-		display: display,
-		fontSize: `${theme.buttons.fontSize}`,
-		border: 'none',
-		textDecoration: linkUnderline,
-		cursor: 'pointer',
-		':visited': {
-			color: color
-		},
-		':hover,:focus': {
-			color: hover,
-			textDecoration: linkUnderlineHover
-		},
-		'a&:not([href])': {
-			color: disabled,
-			cursor: 'not-allowed'
-		}
-	}
+  const css = {
+    color,
+    display,
+    fontSize: `${theme.buttons.fontSize}`,
+    border: 'none',
+    textDecoration: linkUnderline,
+    cursor: 'pointer',
+    ':visited': {
+      color
+    },
+    ':hover,:focus': {
+      color: hover,
+      textDecoration: linkUnderlineHover
+    },
+    'a&:not([href])': {
+      color: disabled,
+      cursor: 'not-allowed'
+    }
+  };
 
-	return css;
-}
+  return css;
+};
 
 const LinkBase: FC<ThemeProps & JSX.IntrinsicElements['a'] & Props> = ({
-	href,
-	disabled,
-	underline = 'none',
-	children,
-	primary,
-	secondary,
-	inverse,
-	asButton,
-	block,
-	theme,
-	css,
-	...rest
+  href,
+  disabled,
+  underline = 'none',
+  children,
+  primary,
+  secondary,
+  inverse,
+  asButton,
+  block,
+  theme,
+  css,
+  ...rest
 }) => {
-	let variant;
-	let baseCss = {};
-	if (primary) {
-		variant = LinkVariant.Primary;
-	} else if (secondary) {
-		variant = LinkVariant.Secondary;
-	}
+  let variant;
+  let baseCss = {};
+  if (primary) {
+    variant = LinkVariant.Primary;
+  } else if (secondary) {
+    variant = LinkVariant.Secondary;
+  }
 
-	if (asButton) {
-		baseCss = getButtonStyles({theme, variant, block});
-	} else {
-		baseCss = getLinkStyles({theme, variant, underline, inverse, block});
-	}
-	baseCss = {...baseCss, ...css};
+  if (asButton) {
+    baseCss = getButtonStyles({ theme, variant, block });
+  } else {
+    baseCss = getLinkStyles({ theme, variant, underline, inverse, block });
+  }
+  baseCss = { ...baseCss, ...css };
 
-	return (
-		<a
-			{...rest}
-			href={disabled ? undefined : href}
-			css={baseCss}
-		>
-			{children}
-		</a>
-	);
+  return (
+    <a {...rest} href={disabled ? undefined : href} css={baseCss}>
+      {children}
+    </a>
+  );
 };
 
 const Link = withTheme(LinkBase);
--- src/lib/components/paper/paper.tsx
+++ src/lib/components/paper/paper.tsx
@@ -1,47 +1,50 @@
 /** @jsx jsx */
 import { ReactNode } from 'react';
-import { jsx, css } from '@emotion/core'
+import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
 
 import { withTheme } from 'emotion-theming';
 import { ThemeInterface } from '../../../theme/theme';
 
 interface Props {
-    className?: string;
-    children: ReactNode;
-    isRaised?: boolean;
-    noPadding?: boolean;
-    as?: 'div' | 'section' | 'article';
-    theme: ThemeInterface;
+  className?: string;
+  children: ReactNode;
+  isRaised?: boolean;
+  noPadding?: boolean;
+  as?: 'div' | 'section' | 'article';
+  theme: ThemeInterface;
 }
 
-const PaperBase = ({ as = 'div', className, children, noPadding, theme, isRaised = false }: Props) => {
-    let boxShadow = 'box-shadow: none';
-    if (isRaised) {
-        boxShadow = `box-shadow: 20px 20px 0 0 rgba(0,0,0,.3)`;
-    }
-    
-    const PaperContainer = styled(as)`
-        display: 'flex';
-        background: ${theme.colors.white};
-        flex-direction: 'column';
-        border-radius: ${theme.borderRadius}px;
-        border: 1px solid ${theme.colors.grey};
-        text-align: 'left';
-        ${boxShadow};
+const PaperBase = ({
+  as = 'div',
+  className,
+  children,
+  noPadding,
+  theme,
+  isRaised = false
+}: Props) => {
+  let boxShadow = 'box-shadow: none';
+  if (isRaised) {
+    boxShadow = `box-shadow: 20px 20px 0 0 rgba(0,0,0,.3)`;
+  }
 
-        ${!noPadding &&
-            css`
-                padding: ${theme.paper.padding};
-        `};
-    `;
+  const PaperContainer = styled(as)`
+    display: 'flex';
+    background: ${theme.colors.white};
+    flex-direction: 'column';
+    border-radius: ${theme.borderRadius}px;
+    border: 1px solid ${theme.colors.grey};
+    text-align: 'left';
+    ${boxShadow};
 
-    return (
-        <PaperContainer className={className}>
-            {children}
-        </PaperContainer>
-    );
+    ${!noPadding &&
+      css`
+        padding: ${theme.paper.padding};
+      `};
+  `;
+
+  return <PaperContainer className={className}>{children}</PaperContainer>;
 };
 
-const Paper = withTheme(PaperBase)
+const Paper = withTheme(PaperBase);
 export default Paper;
