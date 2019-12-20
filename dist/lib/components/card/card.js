06ae5e4c0d530cd49826445808fbae40f22e96ea
Owen Riley <oriley@lucidchart.com>
2019-12-20 15:27:20 -0700

    Moved cardContainer outside of cardBase to avoid constant full rerenders

--- dist/lib/components/card/card.js
+++ dist/lib/components/card/card.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /** @jsx jsx */
 const react_1 = require("react");
 const core_1 = require("@emotion/core");
-const styled_1 = require("@emotion/styled");
 const card_action_1 = require("./card-action");
 const card_actions_1 = require("./card-actions");
 const card_content_1 = require("./card-content");
@@ -11,6 +10,18 @@ const card_image_1 = require("./card-image");
 const card_title_1 = require("./card-title");
 const card_subtitle_1 = require("./card-subtitle");
 const emotion_theming_1 = require("emotion-theming");
+const styled_1 = require("../../../theme/styled");
+const CardContainer = styled_1.default.div({}, ({ theme, horizontal }) => ({
+    display: 'flex',
+    padding: 0,
+    background: theme.colors.white,
+    flexDirection: horizontal ? 'row' : 'column',
+    borderRadius: `${theme.borderRadius}px`,
+    border: `1px solid ${theme.colors.grey}`,
+    textAlign: 'left',
+    textDecoration: 'none',
+    overflow: 'hidden'
+}));
 const CardBase = react_1.forwardRef(({ as = 'div', children, className, horizontal, theme, isRaised = false, isInteractive = false }, ref) => {
     let boxShadow = 'none';
     let isInteractiveProps = {};
@@ -37,18 +48,7 @@ const CardBase = react_1.forwardRef(({ as = 'div', children, className, horizont
         };
     }
     const cardCss = core_1.css(Object.assign({ boxShadow }, isInteractiveProps));
-    const CardContainer = styled_1.default(as) `
-      display: flex;
-      padding: 0;
-      background: ${theme.colors.white};
-      flex-direction: ${horizontal ? 'row' : 'column'};
-      border-radius: ${theme.borderRadius}px;
-      border: 1px solid ${theme.colors.grey};
-      text-align: left;
-      text-decoration: none;
-      overflow: hidden;
-    `;
-    return (core_1.jsx(CardContainer, { css: cardCss, className: className, ref: ref }, children));
+    return (core_1.jsx(CardContainer, { as: as, css: cardCss, className: className, ref: ref, horizontal: horizontal }, children));
 });
 const Card = Object.assign(emotion_theming_1.withTheme(CardBase), {
     Action: card_action_1.default,
--- src/lib/components/card/card.tsx
+++ src/lib/components/card/card.tsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import { ReactNode, forwardRef, SyntheticEvent } from 'react';
 import { jsx, css } from '@emotion/core';
-import styled from '@emotion/styled';
 import { ThemeProps } from '../../../theme/theme';
 
 import CardAction from './card-action';
@@ -12,6 +11,7 @@ import CardTitle from './card-title';
 import CardSubtitle from './card-subtitle';
 
 import { withTheme } from 'emotion-theming';
+import styled from '../../../theme/styled';
 
 interface Props {
   as?: 'div' | 'section' | 'article';
@@ -20,8 +20,19 @@ interface Props {
   horizontal?: boolean;
   isInteractive?: boolean;
   isRaised?: boolean;
-  theme: any;
 }
+
+const CardContainer = styled.div<Props>({}, ({ theme, horizontal }) => ({
+  display: 'flex',
+  padding: 0,
+  background: theme.colors.white,
+  flexDirection: horizontal ? 'row' : 'column',
+  borderRadius: `${theme.borderRadius}px`,
+  border: `1px solid ${theme.colors.grey}`,
+  textAlign: 'left',
+  textDecoration: 'none',
+  overflow: 'hidden'
+}));
 
 const CardBase = forwardRef<HTMLDivElement, Props & ThemeProps>(
   (
@@ -68,20 +79,8 @@ const CardBase = forwardRef<HTMLDivElement, Props & ThemeProps>(
       ...isInteractiveProps
     });
 
-    const CardContainer = styled(as)`
-      display: flex;
-      padding: 0;
-      background: ${theme.colors.white};
-      flex-direction: ${horizontal ? 'row' : 'column'};
-      border-radius: ${theme.borderRadius}px;
-      border: 1px solid ${theme.colors.grey};
-      text-align: left;
-      text-decoration: none;
-      overflow: hidden;
-    `;
-
     return (
-      <CardContainer css={cardCss} className={className} ref={ref}>
+      <CardContainer as={as} css={cardCss} className={className} ref={ref} horizontal={horizontal}>
         {children}
       </CardContainer>
     );
