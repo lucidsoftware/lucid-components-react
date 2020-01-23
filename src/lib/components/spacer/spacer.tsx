90b3fa3555a5158725d2ae69223e25f119f42fe2
Lars Bucio <lbucio@lucidchart.com>
2020-01-23 12:12:08 -0700

    Test spacer

--- src/lib/components/spacer/spacer.tsx
+++ src/lib/components/spacer/spacer.tsx
@@ -1,17 +1,20 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { withTheme } from 'emotion-theming';
-import { ThemeProps } from '../../../theme/theme';
 
-import { SpacerItem, SpacerProps } from './spacer.styles';
+import styled from '../../../theme/styled';
 
-import { FC } from 'react';
+export interface Props {
+  className?: string;
+  size: number;
+}
 
-const SpacerBase: FC<
-  ThemeProps & JSX.IntrinsicElements['div'] & SpacerProps
-> = ({ className, theme, size, ...rest }) => {
-  return <SpacerItem theme={theme} size={size} className={className} />;
-};
+const Spacer = styled.div<Props>(
+  {
+    width: '100%'
+  },
+  props => ({
+    padding: `${props.theme.space[props.size]}px`
+  })
+);
 
-const Spacer = withTheme(SpacerBase);
 export default Spacer;
