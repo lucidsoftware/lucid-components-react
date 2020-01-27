53245a5255285feb7624751e5301fd804389f569
Matt Darrington <mdarrington@lucidchart.com>
2020-01-27 15:00:44 -0700

    updated card content to use 1 1 auto for flex for ie11

--- src/lib/components/card/card-content.tsx
+++ src/lib/components/card/card-content.tsx
@@ -7,7 +7,7 @@ export interface CardContentProps {
 const CardContent = styled('div')<CardContentProps>({}, props => ({
   color: props.theme.colors.text,
   display: 'flex',
-  flex: 1,
+  flex: '1 1 auto',
   flexDirection: 'column',
   justifyContent: 'space-between',
   padding: `${props.noPadding ? 0 : props.theme.card.padding}`
