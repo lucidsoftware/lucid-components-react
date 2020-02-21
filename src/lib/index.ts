548138afd5201e81cd1c64c3288aeecb6a3d65ea
Lars Bucio <lbucio@lucidchart.com>
2020-02-21 10:36:02 -0700

    clean: Add forms to export and clean up order

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,44 +1,37 @@
 import '@emotion/core';
-import Spacer from './components/spacer/spacer';
+
 import Accordion from './components/accordion/accordion';
 import Breadcrumb from './components/breadcrumb/breadcrumb';
 import Button, { getButtonStyles } from './components/button/button';
 import Card from './components/card/card';
+import Field from './components/form/field/field';
 import Icon, { IconType } from './components/icon/icon';
+import Label from './components/form/label/label';
 import Link, { getLinkStyles, LinkVariant } from './components/link/link';
 import Navbar from './components/navbar/navbar';
 import Paper from './components/paper/paper';
-import VideoPlayer from './components/video-player/video-player';
+import Spacer from './components/spacer/spacer';
+import TextInput from './components/form/text-input/text-input';
 import VideoPlayButton from './components/video-play-button/video-play-button';
-
-import Input from './components/input/input';
-
-import {
-  InputValidator,
-  InputDefaultValidator,
-  InputPasswordValidator,
-  InputEmailValidator
-} from './components/input/input-validators';
+import VideoPlayer from './components/video-player/video-player';
 
 export {
   Accordion,
   Breadcrumb,
   Button,
   Card,
-  getLinkStyles,
+  Field,
   getButtonStyles,
+  getLinkStyles,
   Icon,
   IconType,
-  Input,
-  // InputValidator,
-  InputEmailValidator,
-  InputDefaultValidator,
-  InputPasswordValidator,
+  Label,
   Link,
   LinkVariant,
   Navbar,
   Paper,
   Spacer,
+  TextInput,
   VideoPlayButton,
   VideoPlayer
 };
