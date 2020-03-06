6be8ffd7ac2df635997ce8883ced9d5f0fe4dcaf
Lars Bucio <lbucio@lucidchart.com>
2020-03-06 10:46:41 -0700

    [auto] Build component library.

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,7 +1,9 @@
 import '@emotion/core';
 import Accordion from './components/accordion/accordion';
+import Box from './components/box/box';
 import Breadcrumb from './components/breadcrumb/breadcrumb';
 import Button, { getButtonStyles } from './components/button/button';
+import Heading from './components/heading/heading';
 import Card from './components/card/card';
 import Icon, { IconType } from './components/icon/icon';
 import Form from './components/form/form/form';
@@ -11,4 +13,4 @@ import Paper from './components/paper/paper';
 import Spacer from './components/spacer/spacer';
 import VideoPlayButton from './components/video-play-button/video-play-button';
 import VideoPlayer from './components/video-player/video-player';
-export { Accordion, Breadcrumb, Button, Card, Form, getButtonStyles, getLinkStyles, Icon, IconType, Link, LinkVariant, Navbar, Paper, Spacer, VideoPlayButton, VideoPlayer };
+export { Accordion, Box, Breadcrumb, Button, Card, Heading, Form, getButtonStyles, getLinkStyles, Icon, IconType, Link, LinkVariant, Navbar, Paper, Spacer, VideoPlayButton, VideoPlayer };
