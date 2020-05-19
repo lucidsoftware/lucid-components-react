369e0c25e95c0c93b9546f6412d974421f5b5936
Matt Darrington <mdarrington@lucidchart.com>
2020-05-19 15:19:51 -0600

    [auto] Build component library.

--- dist/theme/theme.d.ts
+++ dist/theme/theme.d.ts
@@ -1,11 +1,15 @@
 import * as CSS from 'csstype';
-import { Theme as DefaultTheme } from 'styled-system';
+import { ObjectOrArray, Theme as DefaultTheme } from 'styled-system';
 import { navbar, forms, breadcrumb } from './components';
 export interface Theme extends DefaultTheme {
+    breakpoints: ObjectOrArray<string>;
     colors: Record<string, string>;
     radii: Record<string, string | number>;
     navbar: typeof navbar;
     accordion: Record<'button' | 'panel', Record<string, CSS.StandardProperties | object>>;
+    fontWeights: Record<string, number>;
+    lineHeights: Record<string, number>;
+    letterSpacings: Record<string, string>;
     forms: typeof forms;
     breadcrumb: typeof breadcrumb;
     buttons: any;
