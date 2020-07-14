8c2739b1b6b70d463a9524240adada0dd0ae1cd8
Matt Darrington <mdarrington@lucidchart.com>
2020-07-14 15:13:15 -0600

    [auto] Build component library.

--- dist/utils/utils.js
+++ dist/utils/utils.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.windowIsAboveWidth = (width) => {
-    return window.innerWidth <= parseInt(width);
+    return window.innerWidth >= parseInt(width);
 };
 exports.isIE11 = () => {
     return (typeof navigator !== 'undefined' &&
