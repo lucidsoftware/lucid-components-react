81a41ee83a091ebbb2fd668fd38ba0cc47e6f616
Daniel Soffiantini <dsoffiantini@lucidchart.com>
2020-11-06 13:11:24 -0700

    changes navbar boreder to box shadow

--- dist/theme/components.js
+++ dist/theme/components.js
@@ -30,7 +30,8 @@ exports.navbar = {
     padding: '10px',
     borderColor: 'lightgrey',
     borderWidth: '2px',
-    borderOffset: '-10px',
+    borderOffset: '14px',
+    boxShadow: '0px 3px 4px rgba(40, 44, 51, 0.2)',
     dropdown: {
         padding: '0.5rem 1.5rem',
         mobilePadding: '0.5rem',
