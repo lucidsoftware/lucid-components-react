1d4de3701e9f0b75907a7ba0a4921aa8a42b14a2
Matthew Rumery <mrumery@lucidchart.com>
2021-01-28 17:29:52 -0700

    fix: add jsx back for emotion

--- dist/lib/components/video-player/video-player.js
+++ dist/lib/components/video-player/video-player.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -33,8 +14,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/** @jsx jsx */
 const react_1 = require("@emotion/react");
-const react_2 = __importStar(require("react"));
+const react_2 = require("react");
 const lazy_1 = __importDefault(require("react-player/lazy"));
 const video_play_button_1 = __importDefault(require("../video-play-button/video-play-button"));
 const styled_1 = __importDefault(require("../../../theme/styled"));
@@ -94,17 +76,17 @@ const VideoPlayer = (_a) => {
         borderRadius: '5px',
         paddingTop: ratio === 'wide' ? '56.25%' : '100%'
     });
-    return (react_2.default.createElement("div", { className: containerClassName, css: videoContainerCss },
-        overlayVisible && (react_2.default.createElement(VideoOverlay, null,
-            react_2.default.createElement(video_play_button_1.default, { onClick: () => {
+    return (react_1.jsx("div", { className: containerClassName, css: videoContainerCss },
+        overlayVisible && (react_1.jsx(VideoOverlay, null,
+            react_1.jsx(video_play_button_1.default, { onClick: () => {
                     if (url) {
                         setOverlayVisible(false);
                         setIsPlaying(true);
                     }
                     onClick();
                 }, size: "lg" }))),
-        placeholder && overlayVisible && (react_2.default.createElement(VideoPlaceholder, null, placeholder())),
-        url && (react_2.default.createElement(lazy_1.default, Object.assign({}, rest, { css: reactPlayerCss, 
+        placeholder && overlayVisible && (react_1.jsx(VideoPlaceholder, null, placeholder())),
+        url && (react_1.jsx(lazy_1.default, Object.assign({}, rest, { css: reactPlayerCss, 
             /* eslint-disable-next-line react/jsx-sort-props */
             className: className, height: "100%", playing: isPlaying, url: url, width: "100%" })))));
 };
